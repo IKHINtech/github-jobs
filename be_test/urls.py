0483c1ff2989072a532f189e505734cd40be9cbb
Sarikhin <sarikhin.tubari@agungsedayu.com>
2022-05-10 17:23:00 +0700

    add create user

--- be_test/urls.py
+++ be_test/urls.py
@@ -31,7 +31,7 @@ schema_view = get_schema_view(
         default_version='v1',
         description="GitHub Jobs API documentation",
         terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="bgswdyt@gmail.com"),
+        contact=openapi.Contact(email="saudpakethemat@gmail.com"),
         license=openapi.License(name="BSD License"),
     ),
     public=True,
@@ -42,8 +42,8 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/auth/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('api/', include('be_test.app.urls')),
